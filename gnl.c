5068cbc52448a64999cda23a308023cee6fcbb1a
Beatriz Yoshimori <beatrizyoshimori@MacBook-Air-de-Beatriz.local>
2022-10-03 00:34:22 -0300

    domingo

--- gnl.c
+++ gnl.c
@@ -13,6 +13,33 @@ size_t	ft_strlen(const char *s)
 	return (length);
 }
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sub;
+
+	if (start > ft_strlen(s))
+		sub = malloc(1);
+	else if (len < ft_strlen(s))
+		sub = malloc(len + 1);
+	else
+		sub = malloc(ft_strlen(s) - start + 1);
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	if (start <= ft_strlen(s))
+	{
+		while (s[start] && i < len)
+		{
+			sub[i] = s[start];
+			i++;
+			start++;
+		}
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
 char	*ft_strdup(const char *src)
 {
 	int		i;
@@ -81,15 +108,19 @@ char    *get_next_line(int fd)
     char            *buf;
     char            *str;
     char            *aux;
+    char            *tmp;
     static char     *next_line = 0;
     int             rd;
     int             bytes;
     int             i;
 
-    printf("next_line1: %s\n", next_line);
+    //printf("next_line1: %s\n", next_line);
     bytes = 5;
+    buf = malloc(bytes);
     rd = read(fd, buf, bytes);
-    printf("buf: %s\n", buf);
+    if (rd <= 0)
+        return (NULL);
+    //printf("buf: %s\n", buf);
     i = 0;
     while (i < bytes)
     {
@@ -97,23 +128,43 @@ char    *get_next_line(int fd)
             break ;
         i++;
     }
-    str = malloc(i);
-    ft_strlcpy(str, buf, i);
-    printf("str: %s\n", str);
-    if (next_line != 0)
-    {
-        aux = ft_strjoin(next_line, str);
-        printf("aux: %s\n", aux);
-    }
+    if (rd < bytes)
+        i = rd;
+    printf("i: %d\n", i);
     if (i < bytes)
-        next_line = &buf[i];
+    {
+        str = ft_strjoin(next_line, ft_substr(buf, 0, i));
+        aux = ft_strdup(next_line);
+        next_line = ft_substr(buf, i + 1, bytes - i);
+        printf("line: %s\n", str);
+        //printf("next_line: %s\n", next_line);
+    }
     else
     {
-        next_line = buf;
-        printf("next_line: %s\n", next_line);
-        get_next_line(fd);
+        aux = ft_strdup(next_line);
+        next_line = ft_strjoin(aux, buf);
+        //printf("next_line(else): %s\n", next_line);
+        str = ft_strjoin(buf, get_next_line(fd));
+        //printf("line(else): %s\n", str);
     }
-    return (aux);
+    return (str);
+    // str = ft_strjoin(next_line, buf);
+    // //ft_strlcpy(str, buf, i + 1);
+    // printf("str: %s\n", str);
+    // if (next_line != 0)
+    // {
+    //     aux = ft_strjoin(next_line, str);
+    //     printf("aux: %s\n", aux);
+    // }
+    // if (i < bytes)
+    //     next_line = &buf[i];
+    // else
+    // {
+    //     next_line = buf;
+    //     printf("next_line: %s\n", next_line);
+    //     get_next_line(fd);
+    // }
+    // return (aux);
 }
 
 int main(void)
@@ -121,5 +172,7 @@ int main(void)
     int fd;
 
     fd = open("teste", O_RDONLY);
-    printf("%s\n", get_next_line(fd));
+    get_next_line(fd);
+    get_next_line(fd);
+    get_next_line(fd);
 }
