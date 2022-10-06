1dd995dbce3c1d51942ccf9c1488c8d1ae9e878d
byoshimo <byoshimo@student.42sp.org.br>
2022-10-06 04:47:40 +0200

    quarta noite

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 01:49:36 by byoshimo          #+#    #+#             */
-/*   Updated: 2022/10/05 21:59:33 by byoshimo         ###   ########.fr       */
+/*   Updated: 2022/10/06 04:13:30 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	return (t);
 }
 
-char	*ft_substr(char const *s, int start, int len)
+char	*ft_substr(char *s, int start, int len)
 {
 	int		i;
 	char	*sub;
@@ -68,6 +68,34 @@ char	*ft_substr(char const *s, int start, int len)
 	return (sub);
 }
 
+char	*ft_substr_free(char *s, int start, int len)
+{
+	int		i;
+	char	*sub;
+
+	if (start > ft_strlen(s))
+		sub = malloc(1);
+	else if (len < ft_strlen(s))
+		sub = malloc(len + 1);
+	else
+		sub = malloc(ft_strlen(s) - start + 1);
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	if (start <= ft_strlen(s))
+	{
+		while (s[start] && i < len)
+		{
+			sub[i] = s[start];
+			i++;
+			start++;
+		}
+	}
+	sub[i] = '\0';
+	free(s);
+	return (sub);
+}
+
 char	*ft_strdup(char *src)
 {
 	int		i;
@@ -90,7 +118,7 @@ char	*ft_strdup(char *src)
 	return (c);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin_free(char *s1, char *s2)
 {
 	int		i;
 	int		j;
@@ -113,52 +141,45 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 	}
 	s3[i] = '\0';
+	free(s1);
+	free(s2);
 	return (s3);
 }
 
 char    *get_next_line(int fd)
 {
-	//char		*buf;
-	char		*str = NULL;
+	char		*str;
 	char		*aux;
-	static char	*next_line = 0;
+	static char	*next_line;
 	int			rd;
 	int			i;
+	int			length;
 
 	str = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!str)
+		return (NULL);
 	rd = read(fd, str, BUFFER_SIZE);
-	// if (rd == 0)
-	// {
-	// 	free(str);
-	// 	str = ft_strdup("");
-	// }
-	if (BUFFER_SIZE <= 0 || rd < 0 || !str || (rd == 0 && (!next_line || ft_strlen(next_line) == 0)))
+	if (BUFFER_SIZE <= 0 || rd < 0 || (rd == 0 && ft_strlen(next_line) == 0))
 	{
 		free(str);
 		return (NULL);
 	}
 	if (next_line == NULL)
 		next_line = ft_strdup("");
-	aux = ft_strjoin(next_line, str);
-	free(str);
-	free(next_line);
-	next_line = ft_strdup(aux);
-	free(aux);
+	next_line = ft_strjoin_free(next_line, str);
+	length = ft_strlen(next_line);
 	i = 0;
-	while (next_line[i] != '\n' && next_line[i] && i < ft_strlen(next_line))
+	while (next_line[i] != '\n' && next_line[i] && i < length)
 		i++;
 	if (next_line[i] == '\n' || rd < BUFFER_SIZE)
 	{
 		str = ft_substr(next_line, 0, i + 1);
-		// if (ft_strlen(str) == 0)
-		// {
-		// 	free(str);
-		// 	//str = NULL;
-		// }
-		aux = ft_substr(next_line, i + 1, ft_strlen(next_line) - i);
-		free(next_line);
-		next_line = ft_strdup(aux);
-		free(aux);
+		next_line = ft_substr_free(next_line, i + 1, length);
+		if (!next_line[0])
+		{
+			free(next_line);
+			next_line = NULL;
+		}
 		return (str);
 	}
 	else
@@ -170,16 +191,19 @@ char    *get_next_line(int fd)
 	return (str);
 }
 
-int main(void)
-{
-    int fd;
-	int	i;
+// int main(void)
+// {
+//     int fd;
+// 	int	i;
+// 	char	*str;
 
-    fd = open("teste", O_RDONLY);
-	i = 0;
-	while (i < 2)
-	{
-		printf("%s", get_next_line(fd));
-		i++;
-	}
-}
+//     fd = open("teste", O_RDONLY);
+// 	i = 0;
+// 	while (i < 2)
+// 	{
+// 		str = get_next_line(fd);
+// 		printf("%s", str);
+// 		free(str);
+// 		i++;
+// 	}
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 01:40:22 by byoshimo          #+#    #+#             */
-/*   Updated: 2022/10/05 21:48:31 by byoshimo         ###   ########.fr       */
+/*   Updated: 2022/10/06 04:46:08 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,10 @@
 #endif
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *src);
-char	*ft_substr(char const *s, int start, int len);
+char	*ft_substr(char *s, int start, int len);
+char	*ft_substr_free(char *s, int start, int len);
 int 	ft_strlen(const char *s);
 
 #endif
